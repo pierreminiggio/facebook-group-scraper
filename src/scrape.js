346773183ea94285a8f67dfd36e6521296c80616
Pierre Miniggio <pierre.miniggio@gmail.com>
2021-01-02 14:35:33 +0100

    set default config

--- src/scrape.js
+++ src/scrape.js
@@ -5,13 +5,21 @@ import asyncForEach from '@pierreminiggio/async-foreach'
 import getPostContent from './getPostContent.js'
 
 /**
+ * @typedef {Object} FacebookGroupScraperConfig
+ * @property {number} scrollLength default 30000
+ * @property {boolean} show default false
+ * 
  * @param {string} login
  * @param {string} password
- * @param {boolean} show 
+ * @param {FacebookGroupScraperConfig} config 
  */
-export default async function (login, password, show = false) {
+export default async function (login, password, config = {}) {
+
+    setDefaultConfig(config, 'scrollLength', 30000)
+    setDefaultConfig(config, 'show', false)
+    
     const browser = await puppeteer.launch({
-        headless: ! show,
+        headless: ! config.show,
         args: [
             '--disable-notifications'
         ]
@@ -34,4 +42,18 @@ export default async function (login, password, show = false) {
     })
 
     browser.close()
+}
+
+/**
+ * 
+ * @param {FacebookGroupScraperConfig} config 
+ * @param {string} configKey 
+ * @param {*} defaultValue
+ * 
+ * @returns void
+ */
+function setDefaultConfig(config, configKey, defaultValue) {
+    if (! (configKey in config)) {
+        config[configKey] = defaultValue
+    }
 }
